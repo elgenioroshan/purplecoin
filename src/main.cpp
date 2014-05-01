faf0f1dec430c1a3f7ed8d422c5d4b25002a6356
Mohammed Roshan <coolron90@gmail.com>
2014-05-01 21:51:15 +0530

    PurpleCoin

--- src/main.cpp
+++ src/main.cpp
@@ -40,8 +40,8 @@ CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // "standard" scrypt target limit
 CBigNum bnProofOfStakeLimit(~uint256(0) >> 20);
 CBigNum bnProofOfWorkLimitTestNet(~uint256(0) >> 16);
 
-unsigned int nTargetSpacing = 40; // 40 second block
-unsigned int nStakeMinAge = 3 * 24 * 60 * 60; // 3 days
+unsigned int nTargetSpacing = 1 * 60; // 1 minute
+unsigned int nStakeMinAge = 8 * 60 * 60; // 8 hours
 unsigned int nStakeMaxAge = -1; // unlimited
 unsigned int nModifierInterval = 10 * 60; // time to elapse before new modifier is computed
 
